36a6bdf5ce4a010306becb13852bc8fbeec6b6ec
Youngjae <youngje3431@gmail.com>
2022-05-15 22:52:10 +0900

    URL분석하기

---
+++ study-cofigURL.js
@@ -0,0 +1,5 @@
+let url = new URL('http://example.com/business/mart/item?category=14&id=2965');
+console.log(url.protocol);
+console.log(url.host);
+console.log(url.pathname);
+console.log(url.search);
