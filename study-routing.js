b950dbfc1a332d681fe991fc036211aed065dda3
Youngjae <youngje3431@gmail.com>
2022-05-15 23:06:46 +0900

    URL 라우팅 스터디

---
+++ study-routing.js
@@ -0,0 +1,20 @@
+const http = require('http');
+
+users = ['Tom', 'Andy', 'Jessics', 'Paul'];
+
+let server = http.createServer(function (request,response){
+  // response.end('<h1>Hello World</h1>');
+  //클라이언트가 요청한 정보는 request 객체에 담겨 있다.
+  // console.log(request.url);
+
+  if(request.url === '/'){
+    response.end('<h1>Welcome!</h1>');
+  }else if(request.url === '/users'){
+    response.end('<h1>'+users+'</h1>');
+  }else{
+    response.end('<h1>Page Note Available</h1>');
+  }
+
+});
+
+server.listen(3001);
